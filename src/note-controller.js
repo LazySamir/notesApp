18f0ce442876dde28c2fbfac1b11d36064ddc692
Samir Soormally <samirsoormally@gmail.com>
2018-09-19 15:01:46 +0100

    adds note-controller

---
+++ src/note-controller.js
@@ -0,0 +1,10 @@
+(function (exports) {
+
+  function NoteController() {
+    document.getElementById("app").innerHTML = "howdy"
+    console.log(document.getElementById("app").innerHTML)
+  }
+
+  NoteController();
+
+})(this)
